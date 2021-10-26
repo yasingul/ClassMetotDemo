712b311fc60cf5153d637e5447b1da21495ca9e1
yasingul <yasngl46@gmail.com>
2021-10-26 21:39:27 +0300

    Add project files.

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class Musteri
+    {
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public long NationalId { get; set; }
+        public int HesapNo { get; set; }
+        public int PhoneNumber { get; set; }
+        public int MyProperty { get; set; }
+        public double Cuzdan { get; set; }
+
+    }
+}
